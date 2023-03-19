10d1875be90b97a05885fcaf1012fffc51b753dd
Renee Mumbi <1723renee@gmail.com>
2023-03-19 10:53:42 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,9 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+   # added rescue_from
+   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   # GET /birds
   def index
     birds = Bird.all
@@ -8,9 +11,11 @@ class BirdsController < ApplicationController
   end
 
   # POST /birds
+   # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
+
   end
 
   # GET /birds/:id
@@ -22,7 +27,8 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    bird.update!(bird_params)
     render json: bird
   end
 
@@ -45,6 +51,9 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
